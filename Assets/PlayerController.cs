b1f481e0f188dc32320ada6e4faf75afe85a8d12
yong-gug <81640500+yong-gug@users.noreply.github.com>
2025-05-11 05:43:08 +0900

    test
    hello

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRigidbody;
+    public float speed = 8f;
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();   
+    }
+    void Update()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
+
+        Vector3 newVelocity = new Vector3(xSpeed, 0f, zSpeed);
+        playerRigidbody.linearVelocity = newVelocity;
+    }
+    public void Die()
+    {
+        gameObject.SetActive(false);
+    }
+}
